4d22abc3b0b50cd2827e20ce9e2a6c720fdb56c2
Ram Jhatu <ramgautam4488@gmail.com>
2024-10-30 17:43:29 +0530

    chitwan commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,10 @@ const Home = () => {
     <Layout>
       <div className="home" style={{ backgroundColor:"skyblue"}} >
         <div className="headerContainer">
-          <h1> Hearing & Speech Caring Center </h1>
-          <p> Best Hearing & Speech Hospital In Nepal</p>
+          <h1> WELCOME TO THE HEARING AND SPEECH CARING CENTER </h1>
+          <p> Our expert team specializes in providing a wide range of Speech and Hearing services tailored to your unique needs. We are committed to helping you achieve optimal health and well-being in a compassionate and supportive environment.
+            Thank You for choosing us for your Speech & Hearing Services!
+          </p>
           <Link to="/menu">
           <Button>
             Book An Appointment 
